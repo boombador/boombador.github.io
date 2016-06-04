bfde08e88df52b19a6561f61b7875eaed15b3e9e
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2016-06-04 13:31:03 -0400

    move shaders into main.js

---
+++ _posts/2016-06-04-particle-systems-with-threejs.md
@@ -0,0 +1,9 @@
+---
+layout: basic
+---
+
+https://aerotwist.com/tutorials/creating-particles-with-three-js/
+
+http://creativejs.com/tutorials/three-js-part-1-make-a-star-field/index.html
+
+http://threejs.org/examples/#webgl_buffergeometry_custom_attributes_particles
--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 
 var camera, scene, renderer;
 var material, mesh;
-var particleSystem, particleUniforms;
+var particleSystem;
 
 var particles = 1000
 
 function setup(canv) {
-
     var W = window.innerWidth, H = window.innerHeight;
     renderer = new THREE.WebGLRenderer({
         canvas: canv
     });
     renderer.setSize( W, H );
-
-    particleUniforms = {
-        color:     { type: "c", value: new THREE.Color( 0xffffff ) }
-        //texture:   { type: "t", value: new THREE.TextureLoader().load( "textures/sprites/spark1.png" ) }
-    };
 
     camera = new THREE.PerspectiveCamera( 50, W/H, 1, 10000 );
     camera.position.set(0, 151, 0);
@@ -34,24 +28,53 @@ function setup(canv) {
 }
 
 function draw() {
-
     requestAnimationFrame( draw );
+    camera.rotation.y += 0.001;
     renderer.render( scene, camera );
 }
 
-function makeParticleSystem(particleCount, radius) {
+var vertexshaderSource = [
+    'attribute float size;',
+    'attribute vec3 customColor;',
+    'varying vec3 vColor;',
+    'void main() {',
+        'vColor = customColor;',
+        'vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );',
+        'gl_PointSize = size * ( 30.0 / -mvPosition.z );',
+        'gl_Position = projectionMatrix * mvPosition;',
+    '}'
+].join("\n");
+
+var fragmentshaderSource = [
+    'uniform vec3 color;',
+    'varying vec3 vColor;',
+    'void main() {',
+        'gl_FragColor = vec4( color * vColor, 1.0 );',
+    '}'
+].join("\n");
+
+function makeParticleMaterial() {
+
+    var particleUniforms = {
+        color:     { type: "c", value: new THREE.Color( 0xffffff ) }
+    };
 
     var shaderMaterial = new THREE.ShaderMaterial( {
 
         uniforms:       particleUniforms,
-        vertexShader:   document.getElementById( 'vertexshader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+        vertexShader:   vertexshaderSource,
+        fragmentShader: fragmentshaderSource,
 
         blending:       THREE.AdditiveBlending,
         depthTest:      false,
         transparent:    true
 
     });
+
+    return shaderMaterial;
+}
+
+function makeParticleSystem(particleCount, radius) {
 
     var geometry = new THREE.BufferGeometry();
 
@@ -63,7 +86,7 @@ function makeParticleSystem(particleCount, radius) {
 
     for ( var i = 0, i3 = 0; i < particleCount; i ++, i3 += 3 ) {
         positions[ i3 + 0 ] = ( Math.random() * 2 - 1 ) * radius;
-        positions[ i3 + 1 ] = ( Math.random() * 2 - 1 ) * radius;
+        positions[ i3 + 1 ] = ( Math.random() * 2 - 1 ) * radius + radius;
         positions[ i3 + 2 ] = ( Math.random() * 2 - 1 ) * radius;
 
         color.setHSL( i / particleCount, 1.0, 0.5 );
@@ -79,6 +102,7 @@ function makeParticleSystem(particleCount, radius) {
     geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
     geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
 
+    var shaderMaterial = makeParticleMaterial();
     var particleSystem = new THREE.Points( geometry, shaderMaterial );
 
     return particleSystem;
